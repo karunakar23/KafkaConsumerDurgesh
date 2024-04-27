25f06ea5f9882e58c8c72a9633f09eb34f1568b9
Karunakar <karunakarmutthi@gmail.com>
2024-04-27 17:06:40 +0530

    third Commit

--- src/main/java/com/example/demo/MyService.java
+++ src/main/java/com/example/demo/MyService.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dto.User;
+
 @Service
 public class MyService {
 	
@@ -22,6 +24,12 @@ public class MyService {
 	@KafkaListener(topics="javatechie-demo",groupId="groupId-2")
 	public void consumer3(String message) {
 		log.info("Consumer 3"+message);
+	}
+	
+	@KafkaListener(topics="karuna",groupId="groupId-2")
+	public void printObjects(User user) {
+		System.out.println(user.toString());
+		log.info(user.toString());
 	}
 
 }
